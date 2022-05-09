b39d49a508af82e727c2d7da27c0c27c583fa545
franciisca <97069198+franciisca@users.noreply.github.com>
2022-05-09 14:57:48 +0900

    Coding challenge 09 of May of 2022
    map array

---
+++ CC 2022-05-09/main.js
@@ -0,0 +1,17 @@
+//Beginner - Lost Without a Map
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e/javascript
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+function maps(arr){
+    return arr.map(n => n*2)
+}
+
+console.log(maps([3,2,1,4,20]));
+
+//works!
