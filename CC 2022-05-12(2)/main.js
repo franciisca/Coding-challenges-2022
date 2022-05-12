132c135b632dcca5b2f0063e1a1b468f2c4430af
franciisca <97069198+franciisca@users.noreply.github.com>
2022-05-12 11:15:07 +0900

    Coding Challenge 12 of May 2022

---
+++ CC 2022-05-12(2)/main.js
@@ -0,0 +1,12 @@
+//Removing String Spaces
+//https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+//Simple, remove the spaces from the string, then return the resultant string.
+
+function removeSpacesString(str){
+    return str.replaceAll(' ', '')
+}
+
+removeSpacesString('hola como estas')
+
+//works!
