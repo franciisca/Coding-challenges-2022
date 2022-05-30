6a65140c6df6a3444a177cf128dbedb163e40119
franciisca <97069198+franciisca@users.noreply.github.com>
2022-05-30 23:07:38 +0900

    Coding challenge 30 of May of 2022

---
+++ CC 2022-05-30/main.js
@@ -0,0 +1,42 @@
+//Categorize New Member
+//https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/javascript
+
+
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+// Input
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+// Output
+// Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+// Example
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+//PREP
+//P: One array that takes integers (negative or positive). Inside the array there's a set of two numbers: the age and the handicap number (input)
+//R: returns an array of strings (output)
+//E: [20,8]  //["Open"]
+//Pseudo: function. the parameter takes an array (list of pairs). 
+//then it checks if age is 55orMore AND the handicap greater than 7  = Senior (if not "Open")
+//(handicaps range from -2 to +26) 
+
+// function openOrSenior([a,b]){
+//     if (a >= 55 && b > 7){
+//         return ["Senior"]
+//     }
+//   }
+
+//   openOrSenior([56,10])
+
+  function openOrSenior(data){
+    
+    if (a >= 55 && b > 7){
+        return ["Senior"]
+    }
+  }
+
+  openOrSenior([56,10])
